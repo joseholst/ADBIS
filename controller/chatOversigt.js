c9a62222ff44e296b06e023390da7e71a648e2f3
Malou Luthcke <malu23ag@student.cbs.dk>
2025-05-10 18:36:56 +0200

    rettelser

--- controller/chatOversigt.js
+++ controller/chatOversigt.js
@@ -15,11 +15,11 @@ const showChatsFromDatabase = async (req, res) => {
         <li class="chat-item">
         <div class="top-row">
           <strong>${chat.Title}</strong><br>
-          <span>${chat.Description && chat.Description.length > 100 
-  ? chat.Description.substring(0, 100) + '...' 
-  : chat.Description || ''}</span> 
+          <span class ="ordre"> Ordre ID: ${chat.ChatID}</span>
+        <div class = "mid">
         <span> Træner: ${chat.Trainer}</span>
         <span> Kategori: ${chat.Category}</span>
+        </div>
         </div>
         <div class="right-column">
           <span class="status">${chat.MessageStatus || ''}</span>
@@ -41,3 +41,7 @@ const showChatsFromDatabase = async (req, res) => {
 };
 
 module.exports.showChatsFromDatabase = showChatsFromDatabase;
+
+/*<span>${chat.Description && chat.Description.length > 100 
+  ? chat.Description.substring(0, 100) + '...' 
+  : chat.Description || ''}</span> */
--- model/classes/Chat.js
+++ model/classes/Chat.js
@@ -39,7 +39,7 @@ class Chat {
             await sql.connect(config);
             const request = new sql.Request();
     
-            const result = await request.query('SELECT Title, Description, Trainer, Category, MessageStatus FROM Chat WHERE UserID = 1');
+            const result = await request.query('SELECT ChatID, Title, Description, Trainer, Category, MessageStatus FROM Chat WHERE UserID = 1');
     
             console.log('Chat view:', result.recordset);
             return result.recordset;
